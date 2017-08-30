7e927e9d9faf6748a507e7bddd83a6e5883818c4
Yan MATAGNE <ymatagne@norsys.fr>
2017-08-30 14:26:54 -0400

    feat(generator) : Add Angular Application, create and replace in files

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,86 @@
 var Generator = require('yeoman-generator');
+var path = require('path');
+const { spawn } = require('child_process');
+
+var files = require('./files.json');
+
 module.exports = class extends Generator {
-  method1() {
-    this.log('method 1 just ran');
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument('compagnyName', {
+      type: String,
+      required: true
+    });
+    this.argument('destWrite', {
+      type: String,
+      required: true
+    });
   }
 
-  method2() {
-    this.log('method 2 just ran');
+  information() {
+    this.log('I\'ll generate wep app with this informations :');
+    this.log('Name of compagny is ' + this.options.compagnyName);
+    this.log('Sources of projects are in folder ' + path.join(this.options.destWrite, this.options.compagnyName));
+  }
+
+  writeSrcApp() {
+    for (var i = 0; i < files.static.length; i++) {
+      var src = path.join(
+        this.sourceRoot(),
+        files.static[i]);
+      var dest = path.join(
+        this.options.destWrite,
+        this.options.compagnyName,
+        files.static[i])
+      console.log('copy ' + src + ' to' + dest);
+      this.fs.copy(
+        this.templatePath(src),
+        this.destinationPath(dest)
+      );
+    }
+    for (var i = 0; i < files.templates.length; i++) {
+      var src = path.join(
+        this.sourceRoot(),
+        files.templates[i]);
+      var dest = path.join(
+        this.options.destWrite,
+        this.options.compagnyName,
+        files.templates[i].replace(/\/_/g, "/"))
+      this.fs.copyTpl(
+        this.templatePath(src),
+        this.destinationPath(dest),
+        { compagnyName: this.options.compagnyName }
+      );
+    };
+  }
+
+  runNpm() {
+    const npm = spawn(
+      'npm',
+      ['install'],
+      {
+        cwd: path.join(
+          this.options.destWrite,
+          this.options.compagnyName)
+      });
+
+    npm.stdout.on('data', (data) => {
+      console.log(`${data}`);
+    });
+  }
+
+  buildProdApplication()  {
+    const ng = spawn(
+      'ng',
+      ['build','--prod'],
+      {
+        cwd: path.join(
+          this.options.destWrite,
+          this.options.compagnyName)
+      });
+
+    ng.stdout.on('data', (data) => {
+      console.log(`${data}`);
+    });
   }
 };
