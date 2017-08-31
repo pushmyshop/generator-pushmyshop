930e193fcd331048f47f4717c00b17294ecdefd6
Antoine Stalin <astalin@norsys.fr>
2017-08-31 21:48:56 +0200

    feat(routing) : add routing to cart product/cart page

--- generators/app/templates/src/app/app.module.ts
+++ generators/app/templates/src/app/app.module.ts
@@ -3,13 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CustomerService } from './service/customer.service';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+
+export const routes: Routes = [
+  { path: '',
+    redirectTo: '/product',
+    pathMatch: 'full'
+  },
+  { path: 'product', component: ProductComponent },
+  { path: 'cart', component: CartComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [CustomerService],
   bootstrap: [AppComponent]
