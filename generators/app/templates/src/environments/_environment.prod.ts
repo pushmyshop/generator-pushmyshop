43908bcf5e7a9902eb927176c51f71e760dbaf40
Yan MATAGNE <ymatagne@norsys.fr>
2017-09-27 16:50:33 -0400

    fix(prod) : drop builder in docker

--- generators/app/templates/src/environments/_environment.prod.ts
+++ generators/app/templates/src/environments/_environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  compagnyUrl : 'https://'+compagnyName+'pushmyshop.com/api/compagnies/<%= compagnyId %>',
+  compagnyUrl : 'https://<%=compagnyName%>.pushmyshop.com/api/compagnies/<%= compagnyId %>',
   vapidPublicKey : 'BGgM0ydbnR-04QSrQyPMC0JMqA7xF6TSQ8Wb7rtKa5PSTdOjpSmdydadrrlwuPziFhUM8DodMt_t09nDi7oFtSY'
 };
